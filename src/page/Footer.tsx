74d16a976c3be3cc1f2fd2f2a2dfdacddafa5705
iamshakil19 <shakilahmed.pure@gmail.com>
2023-07-15 04:28:42 +0600

    footer added

--- src/page/Footer.tsx
+++ src/page/Footer.tsx
@@ -1,39 +1,28 @@
 export default function Footer() {
   return (
-    <div className="container mx-auto mt-20">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-      corporis delectus eaque doloribus facere commodi tempore officia excepturi
-      consequuntur molestiae voluptas veritatis impedit minus nobis perferendis
-      incidunt consequatur saepe expedita soluta sequi, aspernatur aliquam
-      laborum error. Molestias deleniti provident possimus voluptatem cupiditate
-      nesciunt tempora nobis enim, consequatur veritatis temporibus. Eligendi
-      fugiat, delectus earum totam aut tempora hic consequatur et autem ab esse
-      provident odit error, dolores perspiciatis ducimus vel porro quaerat
-      repudiandae illum. Error, excepturi laudantium officia maiores veniam qui
-      adipisci! Optio veniam illo provident sequi pariatur blanditiis labore
-      maxime debitis tenetur dolor accusamus rem quis magni quidem illum laborum
-      veritatis dolorum ipsum doloremque quibusdam autem nesciunt, omnis minima
-      ab! Fuga quibusdam deserunt sapiente qui nobis ipsam sit perspiciatis
-      molestias similique amet, mollitia eum exercitationem voluptate nulla?
-      Vero neque vitae delectus quisquam voluptates molestiae sequi distinctio
-      amet adipisci aut consequuntur nostrum voluptas hic odit impedit libero
-      nesciunt reiciendis id recusandae modi, beatae quae sapiente maxime?
-      Libero voluptatum error similique vero quas at, eum sit perspiciatis
-      consectetur possimus minus. Nihil officiis assumenda suscipit adipisci
-      neque eaque laudantium harum deserunt nobis. Minima, velit veritatis!
-      Consectetur ab ullam totam ipsam quis vero libero laborum optio soluta,
-      commodi nulla iste officia reprehenderit laboriosam accusamus. Placeat ea
-      dolorem vero perspiciatis eaque, saepe numquam velit et harum molestiae
-      incidunt molestias magni! Dolore, recusandae ipsum quo quisquam modi
-      fugit! Atque obcaecati nulla voluptatem nihil quam earum autem modi
-      cupiditate dicta ut ratione, fuga eius, amet inventore numquam, molestiae
-      aperiam. Libero animi, non ex odio esse fuga aspernatur excepturi eligendi
-      dignissimos nisi, distinctio, nobis quis ea facere velit voluptatibus
-      aperiam officia dolore sapiente debitis. Dolorem dignissimos suscipit modi
-      minima possimus sequi. Obcaecati corporis alias itaque sint deserunt et
-      numquam cupiditate, amet omnis reprehenderit, rerum unde quas tempore
-      quod. Veritatis laborum id necessitatibus ducimus? Voluptatibus nisi
-      ducimus iure deleniti.
+    <div className="bg-neutral mt-20">
+      <footer className="footer container mx-auto p-10 text-neutral-content">
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
     </div>
   );
 }
